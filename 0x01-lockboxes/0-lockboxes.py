c638c1c3808506f844a15070831fe2ef92cfc674
Tom-254 <211mseeratiego@gmail.com>
2023-03-31 02:41:30 +0000

    0x01. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Unlock boxes """
+
+
+def canUnlockAll(boxes):
+    """
+    Check if box can be unlocked
+    """
+    if len(boxes) == 0 or type(boxes) is not list:
+        return False
+    locked = {box: True for box in range(1, len(boxes))}
+    locked.update({0: False})
+    keys = boxes[0]
+    unlock(boxes, locked, keys)
+
+    if True in locked.values():
+        return False
+    return True
+
+
+def unlock(boxes, locked, keys):
+    """
+        Recursively unlock the boxes locked
+    """
+    for key in keys:
+        try:
+            if locked[key]:
+                locked[key] = False
+                unlock(boxes, locked, boxes[key])
+        except KeyError:
+            pass
