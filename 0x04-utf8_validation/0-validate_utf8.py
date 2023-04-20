3a6105ec5dea715605733dd829d4715f72beee05
Tom-254 <211mseeratiego@gmail.com>
2023-04-20 22:39:40 +0000

    0x04. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """
 UTF-8 validation module.
+Determines if the given data set represents
+a valid UTF-8 encoding.
 """
 
 
 def validUTF8(data):
     """
+    Determines if the given data set represents a valid UTF-8 encoding.
+
+    Parameters:
+    data (list of int): The data set to check.
+
     Returns True if data is a valid UTF-8 encoding, else False.
     """
     i = 0
