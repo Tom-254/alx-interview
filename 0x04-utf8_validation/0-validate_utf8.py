47121242872296bc33a2ca00f78644cba42a8cf7
Tom-254 <211mseeratiego@gmail.com>
2023-04-20 22:23:51 +0000

    0x04. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,35 +14,28 @@ def validUTF8(data):
     Returns:
     bool: True if the data is valid UTF-8 encoded, False otherwise.
     """
-    if not isinstance(data, bytes):
-        i = 0
-        while i < len(data):
-            if (data[i] & 0b10000000) == 0b00000000:
-                i += 1
-            elif (data[i] & 0b11100000) == 0b11000000:
-                if (data[i+1] & 0b11000000) == 0b10000000:
-                    i += 2
-                else:
-                    return False
-            elif (data[i] & 0b11110000) == 0b11100000:
-                if (data[i+1] & 0b11000000) == 0b10000000 and \
-                   (data[i+2] & 0b11000000) == 0b10000000:
-                    i += 3
-                else:
-                    return False
-            elif (data[i] & 0b11111000) == 0b11110000:
-                if (data[i+1] & 0b11000000) == 0b10000000 and \
-                   (data[i+2] & 0b11000000) == 0b10000000 and \
-                   (data[i+3] & 0b11000000) == 0b10000000:
-                    i += 4
-                else:
-                    return False
+    i = 0
+    while i < len(data):
+        if (data[i] & 0b10000000) == 0b00000000:
+            i += 1
+        elif (data[i] & 0b11100000) == 0b11000000:
+            if (data[i+1] & 0b11000000) == 0b10000000:
+                i += 2
             else:
                 return False
-        return True
-    else:
-        try:
-            data.encode('utf-8').decode('utf-8')
-            return True
-        except UnicodeDecodeError:
+        elif (data[i] & 0b11110000) == 0b11100000:
+            if (data[i+1] & 0b11000000) == 0b10000000 and \
+                    (data[i+2] & 0b11000000) == 0b10000000:
+                i += 3
+            else:
+                return False
+        elif (data[i] & 0b11111000) == 0b11110000:
+            if (data[i+1] & 0b11000000) == 0b10000000 and \
+                (data[i+2] & 0b11000000) == 0b10000000 and \
+                    (data[i+3] & 0b11000000) == 0b10000000:
+                i += 4
+            else:
+                return False
+        else:
             return False
+    return True
