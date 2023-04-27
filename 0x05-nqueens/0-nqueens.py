0822290299f7c9caf56d0e8f1c19fa642586a34b
Tom-254 <211mseeratiego@gmail.com>
2023-04-27 22:14:07 +0000

    0x05. N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -38,6 +38,7 @@ def nqueens(n):
         List[List[int]]: A list of every possible
         solution to the N-Queens puzzle.
     """
+    # Check that n is an integer greater or equal to 4
     if not isinstance(n, int):
         print("N must be a number")
         sys.exit(1)
@@ -45,9 +46,11 @@ def nqueens(n):
         print("N must be at least 4")
         sys.exit(1)
 
+    # Initialize the board
     board = [['.' for _ in range(n)] for _ in range(n)]
     solutions = []
 
+    # Recursive function to place queens
     def place_queen(row, col):
         """Recursively place queens on the board.
 
@@ -55,6 +58,7 @@ def nqueens(n):
             row (int): The row to place the queen in.
             col (int): The column to place the queen in.
         """
+        # Check if this position is attacked by any previous queens
         for i in range(row):
             if board[i][col] == 'Q':
                 return False
@@ -63,8 +67,10 @@ def nqueens(n):
             if col + row - i < n and board[i][col + row - i] == 'Q':
                 return False
 
+        # Place the queen
         board[row][col] = 'Q'
 
+        # If this is the last row, add the solution to the list
         if row == n - 1:
             solution = []
             for i in range(n):
@@ -73,11 +79,14 @@ def nqueens(n):
                         solution.append([i, j])
             solutions.append(solution)
         else:
+            # Try placing a queen in the next row
             for i in range(n):
                 place_queen(row + 1, i)
 
+        # Remove the queen
         board[row][col] = '.'
 
+    # Try placing a queen in each column of the first row
     for i in range(n):
         place_queen(0, i)
 
