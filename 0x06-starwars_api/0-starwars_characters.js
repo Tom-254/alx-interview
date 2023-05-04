ed248a5abc7ec4cb45588e80a5e313b02dd3b5dd
Tom-254 <211mseeratiego@gmail.com>
2023-05-04 20:36:18 +0000

    0x06. Star Wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const get = url => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, _, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+};
+
+get(`https://swapi-api.hbtn.io/api/films/${movieId}/`)
+  .then(response => Promise.all(response.characters.map(url => get(url))))
+  .then(responses => responses.map(response => response.name))
+  .then(names => names.forEach(name => console.log(name)))
+  .catch(error => console.error(error));
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,1 @@
+# ALX Programme Project: 0x06. Star Wars API
