fe136180def62d762683bda5a5b97a63c67cd9f7
Tom-254 <211mseeratiego@gmail.com>
2023-05-18 21:53:01 +0000

    0x08. Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,18 +6,21 @@ to meet a given amount total.
 
 def makeChange(coins, total):
     """Determine the fewest number of coins needed
-to meet a given amount total.
-"""
+    to meet a given amount total.
+    """
     if total <= 0:
         return 0
 
-    max_value = float('inf')
-    dp = [max_value] * (total + 1)
+    max_value = total + 1
+    dp = [max_value] * max_value
     dp[0] = 0
 
-    for amount in range(1, total + 1):
+    for i in range(1, max_value):
         for coin in coins:
-            if coin <= amount:
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    return dp[total] if dp[total] != max_value else -1
+    if dp[total] == max_value:
+        return -1
+    else:
+        return dp[total]
