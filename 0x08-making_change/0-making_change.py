500fb770dd6e51b863890229dce9b1e80a050574
Tom-254 <211mseeratiego@gmail.com>
2023-05-18 21:38:47 +0000

    0x08. Making Change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Determine the fewest number of coins needed
+to meet a given amount total.
+"""
+
+
+def makeChange(coins, total):
+    """Determine the fewest number of coins
+    needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
+
+    memo = {}
+
+    def change(amount):
+        if amount < 0:
+            return float('inf')
+        if amount == 0:
+            return 0
+
+        if amount in memo:
+            return memo[amount]
+
+        min_coins = float('inf')
+        for coin in coins:
+            remaining_coins = change(amount - coin)
+            min_coins = min(min_coins, remaining_coins + 1)
+
+        memo[amount] = min_coins
+        return min_coins
+
+    result = change(total)
+
+    if result == float('inf'):
+        return -1
+    else:
+        return result
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,1 @@
+# ALX Programme Project: 0x08. Making Change
