7d2e4008d5477e6cfde6a7c7983324289fcf4b0e
Tom-254 <211mseeratiego@gmail.com>
2023-05-18 21:43:05 +0000

    0x08. Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+makeChange = __import__('0-making_change').makeChange
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
+
 """Determine the fewest number of coins needed
 to meet a given amount total.
 """
